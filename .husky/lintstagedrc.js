efbdc19aa6192027f742e1ef52b06ccd32e457ea
Aleo <liulh@wasu.com>
2025-08-17 23:06:51 +0800

    feat: 删除变态检查

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,8 +1,8 @@
 export default {
-  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
+  "*.{js,jsx,ts,tsx}": ["prettier --write"],
   "!(package)*.json": ["prettier --write--parser json"],
   "package.json": ["prettier --write"],
-  "*.vue": ["eslint --fix", "prettier --write"],
+  "*.vue": ["prettier --write"],
   "*.{vue,css,scss,postcss,less}": ["prettier --write"],
   "*.md": ["prettier --write"],
 };
